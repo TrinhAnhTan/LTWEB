b17a22930c8821fa3c7104b3ee057304b077ddb4
TrinhAnhTan <nhocconnhinhanh2016@gmail.com>
2024-11-14 09:28:04 +0700

    Delete De5Kiemtra directory

--- De5Kiemtra/src/main/java/com/example/kiemtra/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.kiemtra.repository;
-
-import com.example.kiemtra.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
